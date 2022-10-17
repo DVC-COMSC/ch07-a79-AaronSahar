51261bd7f69e0aaf1f751ccedd7564a09ba3b410
Aaron Sahar <aaronsahar@gmail.com>
2022-10-17 12:53:19 -0700

    test

--- main.py
+++ main.py
@@ -13,6 +13,34 @@ for i in range(rnum):
 	print (rsum, end=' ')
 print()
 
+print ('Sum of columns: ', end=' ')
+for i in range(cnum):
+    csum = 0
+    for j in range(rnum):
+        csum += numbers[j][i]
+    print (csum, end=' ')
+print()
+
+print ('The row that has the greatest sum: ', end=' ')
+if sum(numbers[0]) > sum(numbers[1]) and sum(numbers[0]) > sum(numbers[2]):
+    print('0')
+elif sum(numbers[1]) > sum(numbers[0]) and sum(numbers[1]) > sum(numbers[2]):
+    print('1')
+else:
+    print('2')
+
+print ('The greatest number: ', end=' ')
+greatest = 0
+for i in range(cnum):
+    for j in range(rnum):
+        if numbers[j][i] > greatest:
+            greatest = numbers[j][i]
+print(greatest)
+
+
+
+
+
 # ******************************
 # Make your Code
 # ******************************
